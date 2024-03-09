6499e7568d1f60e09631395d04d8aa10a9421c2b
pnoken <norteypeter@gmail.com>
2024-03-09 05:47:01 +0000

    feat: setup fiatsend documentation

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+    logo: <img src="/favicon.ico" alt="fiatsend logo" width={40} />,
+    project: {
+        link: 'https://github.com/fiatsend'
+    },
+    // editLink: {
+    //     component: {
+    //         file
+    //     }
+    // }
+    // ... other theme options
+}
